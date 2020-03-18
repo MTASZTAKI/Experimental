fb88e10ba1dafbcde5728f8c58b0537a851bb9ab
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2020-03-18 14:01:12 +0100

    hu
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

--- VLFT_Installer/stringReplacer@config.ps1
+++ VLFT_Installer/stringReplacer@config.ps1
@@ -17,17 +17,17 @@ Get-ChildItem . config.json -rec | ForEach-Object {
 }
 
 Get-ChildItem . config.json -rec | ForEach-Object { 
-    Replace-TextInFile -FilePath $_.FullName -Pattern '-build/bin' -Replacement "/bin/release" 
+    Replace-TextInFile -FilePath $_.FullName -Pattern '-build' -Replacement '' 
 }
 
-Get-ChildItem . *.json -rec | ForEach-Object { 
+Get-ChildItem . apeCore.json -rec | ForEach-Object { 
     Replace-TextInFile -FilePath $_.FullName -Pattern '/samples/virtualLearningFactory/resources' -Replacement $($pwd.Path.replace( '\', '/')  + '/samples/virtualLearningFactory/resources') 
 }
 
-Get-ChildItem . *.json -rec | ForEach-Object { 
+Get-ChildItem . apeCore.json -rec | ForEach-Object { 
     Replace-TextInFile -FilePath $_.FullName -Pattern '/macros/sceneMaker/resources' -Replacement $($pwd.Path.replace( '\', '/')  + '/macros/sceneMaker/resources')
 }
 
-Get-ChildItem . *.json -rec | ForEach-Object { 
+Get-ChildItem . apeCore.json -rec | ForEach-Object { 
     Replace-TextInFile -FilePath $_.FullName -Pattern '/plugins/render/ogreRender/resources' -Replacement $($pwd.Path.replace( '\', '/') + '/plugins/render/ogreRender/resources')
 }
